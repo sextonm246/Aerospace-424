7740faf6e527384d0cccc8e82ae7183a57467b57
sextonm246 <mrs6497@psu.edu>
2024-02-21 13:35:03 -0500

    More updates

--- Homework 1/Questions 2 to/Questions 2 to/Questions 2 to.cpp
+++ Homework 1/Questions 2 to/Questions 2 to/Questions 2 to.cpp
@@ -4,11 +4,9 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <unordered_map>
+#include <map>
 using namespace std;
 
-using flightdistances = unordered_map<string, int>; // Container for Question 2
-using airportmap = unordered_map<string, flightdistances>; // Container for Question 2
 
 // Class Plane is for Question 3
 class Plane {
@@ -16,74 +14,79 @@ private:
     double pos; double vel; double distance;
     bool at_SCE;
     string origin; string destination;
-    airportmap flightdistance;
+    map<pair< string,string>,int>flightdistance; // Question 2
 public:
-    Plane(const string& from, const string& to) {
-        distance = flightdistance[from][to];
+    Plane(const string& from, const string& to) : origin(from), destination(to) {
         pos = 0; vel = 0; at_SCE = 0;
         cout << "Plane Created at " << this << endl;
+        flightdistance[{"SCE", "PHL"}] = 160; // Question 2
+        flightdistance[{"SCE", "ORD"}] = 640;
+        flightdistance[{"SCE", "EWR"}] = 220;
+        auto check = flightdistance.find({ origin, destination });
+        if (check != flightdistance.end())
+        {
+            distance = check->second;
+            cout << "Distance between " << origin << " and " << destination << ": " << distance << " miles" << endl;
+        }
     }
     ~Plane() {
         cout << "Plane Destroyed" << endl;
     }
-    void operate(double dt);
-    double getpos() const;
-    double getvel() const;
-    void setvel(double velocity);
-    string getorigin() const;
-    string getdestination() const;
-    bool getatSCE() const;
+    void operate(double dt) {
+        string dummy;
+        if (pos < distance) {
+            pos += vel * (dt/3600);
+            at_SCE = 0;
+        }
+        else {
+            if (destination == "SCE") {
+                at_SCE = 1;
+            }
+            dummy = origin;
+            origin = destination;
+            destination = dummy;
+            pos = 0.0;
+        }
+    }
+    double getpos() const {
+        return pos;
+    }
+    double getvel() const {
+        return vel;
+    }
+    void setvel(double velocity) {
+        vel = velocity;
+    }
+    string getorigin() const {
+        return origin;
+    }
+    string getdestination() const {
+        return destination;
+    }
+    bool getatSCE() const {
+        return at_SCE;
+    }
 };
 
-void Plane::operate(double dt) {
-    string dummy;
-    if (pos < distance) {
-        pos += vel * dt;
-        at_SCE = 0;
-    }
-    else {
-        if (destination == "SCE") {
-            at_SCE = 1;
 
-        }
-        dummy = origin;
-        origin = destination;
-        destination = dummy;
-        pos = 0.0;
-    }
-}
-double Plane::getpos() const {
-    return pos;
-}
-double Plane::getvel() const {
-    return vel;
-}
-void Plane::setvel(double velocity) {
-    vel = velocity;
-}
-string Plane::getorigin() const {
-    return origin;
-}
-string Plane::getdestination() const {
-    return destination;
-}
-bool Plane::getatSCE() const {
-    return at_SCE;
-}
 
 
 
 int main()
 {
     // Question 2:
-
-    airportmap flightdistance;
+    Plane airplane("SCE", "PHL");
 
     // Setting distances for each flight possibility
-    flightdistance["SCE"] = { {"PHL", 160}, {"ORD", 640}, {"EWR", 220} };
-    flightdistance["PHL"] = { {"SCE", 160} };
-    flightdistance["ORD"] = { {"SCE", 640} };
-    flightdistance["EWR"] = { {"SCE", 220} };
+    double velocity = 425;
 
+    airplane.setvel(velocity);
+
+    for (int i = 0; i <= 1000; i++) {
+        double timestep = 25;
+        airplane.operate(timestep);
+        int time = timestep * i;
+        cout << "Time: " << time << " seconds. Position: " << airplane.getpos() << " miles." << endl;
+    }
     return 0;
 }
